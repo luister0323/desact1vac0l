ad92a882728e95d737f228c560e624f0fe661a48
luister0323 <145378234+luister0323@users.noreply.github.com>
2023-09-18 12:37:44 -0500

    Update env.php

--- metronic/include/env.php
+++ metronic/include/env.php
@@ -1,9 +1,9 @@
 <?php
 $env = [
-    'DB_HOST' => 'containers-us-west-54.railway.app',
+    'DB_HOST' => 'containers-us-west-187.railway.app',
     'DB_NAME' => 'railway',
     'DB_USER' => 'root',
-    'DB_PASS' => '5wYSwUteOg5UcTvlGsmP',
-    'DB_PORT' => '7618'
+    'DB_PASS' => 'yI6CFYHER5ifUJCZx5n8',
+    'DB_PORT' => '6797'
 ];
 ?>
